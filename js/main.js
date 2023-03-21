4381010b77f52f74d6d922ba8b7c3320c1b6ce0c
raulrodmo <raulrm.dev@gmail.com>
2023-03-21 00:00:52 -0300

    add .js and desktop version

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 const openMenu = document.querySelector(".buttonOpenMenu")
 const closeMenu = document.querySelector(".buttonCloseMenu")
 const navMenu = document.querySelector (".navMenu")
+const containerMenu =  document.querySelector(".containerSix")
 
 openMenu.addEventListener('click', function() {
-  openMenu.classList.add(".hide")
-  closeMenu.classList.remove(".hide")
-  navMenu.classList.remove(".hide")
+  openMenu.classList.add("hide")
+  containerMenu.classList.remove("hide")
+  closeMenu.classList.remove("hide")
+  navMenu.classList.remove("hide")
+
 
 })
 
 closeMenu.addEventListener('click', function() {
-  openMenu.classList.remove(".hide")
-  closeMenu.classList.add(".hide")
-  navMenu.classList.add(".hide")
+  openMenu.classList.remove("hide")
+  containerMenu.classList.add("hide")
+  closeMenu.classList.add("hide")
+  navMenu.classList.add("hide")
 })
 
 
